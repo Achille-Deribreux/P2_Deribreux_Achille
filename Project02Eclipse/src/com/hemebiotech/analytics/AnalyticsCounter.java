4d93b77f7320945ed364be830c9bd4bac3c12d1d
Achille Deribreux <achille@deribreux.be>
2021-09-02 10:31:29 +0200

    set filepath in const

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -4,11 +4,14 @@ package com.hemebiotech.analytics;
 public class AnalyticsCounter {
 	
 	public static void main(String args[]) throws Exception {
+	
+		final String ENTRY_PATH = "./Project02Eclipse/symptoms.txt";
+		final String RESULT_PATH = "result.out";
 		
-		
-		ReadSymptomDataFromFile read = new ReadSymptomDataFromFile("./Project02Eclipse/symptoms.txt");
+		ReadSymptomDataFromFile read = new ReadSymptomDataFromFile(ENTRY_PATH);
 		Counter count = new Counter(read.GetSymptoms());
-		Writer write = new Writer();
+		Writer write = new Writer(RESULT_PATH);
+		
 		write.Write(count.CountOccurences());
 	}
 }
--- Project02Eclipse/src/com/hemebiotech/analytics/Writer.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Writer.java
@@ -5,11 +5,16 @@ import java.io.IOException;
 import java.util.Map;
 
 public class Writer {
+	private String filePath;
+	
+	public Writer(String filePath) {
+		this.filePath = filePath;
+	}
 	
 	public void Write(Map <String, Integer> sortedMap) {
 		
 		try {
-			FileWriter writer = new FileWriter ("result.out");
+			FileWriter writer = new FileWriter (filePath);
 			
 			for (Map.Entry <String, Integer> mapentry : sortedMap.entrySet()) {
 	          writer.write(mapentry.getKey() + " "+ mapentry.getValue()+"\n");
